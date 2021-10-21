0ed413ac8ecd8f61a4d58265e2fcd4f98b00abea
Allan Haggett <allan.haggett@gov.bc.ca>
2021-10-21 13:17:45 -0700

    Restrict search results to courses on (no blog posts)

--- functions.php
+++ functions.php
@@ -6,3 +6,25 @@ function twentytwentyone_styles() {
 	array( 'twenty-twenty-one-style' ), wp_get_theme()->get('Version') );
 }
 add_action( 'wp_enqueue_scripts', 'twentytwentyone_styles');
+
+/**
+ * Modify search query
+ *
+ * @param WP_Query $query
+ *
+ * @return void
+ */
+function theme_modify_search( $query ) {
+ 
+    if( is_admin() ) {
+		return;
+	}
+		
+	if( $query->is_main_query() ) {
+		if( isset( $_GET, $_GET['post_type'] ) ) {
+			$query->set( 'post_type', $_GET['post_type'] );
+		}
+	}
+ 
+}
+add_action( 'pre_get_posts', 'theme_modify_search' );
